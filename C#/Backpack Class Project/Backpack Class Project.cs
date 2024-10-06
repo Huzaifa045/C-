5ba50499c7aaed024dba5bf61eee40aa56e83732
Huzaifa045 <huzaifaaa.045@gmail.com>
2024-10-05 22:53:54 -0600

    Add files via upload

---
+++ C#/Backpack Class Project/Backpack Class Project.cs
@@ -0,0 +1,103 @@
+﻿#nullable disable
+
+// Backpack Class Assignment
+
+// Backpack1
+Backpack bluebackpack=new Backpack("Blue","small");
+// Backpack2
+Backpack redbackpack=new Backpack("Red","medium");
+// Backpack3
+Backpack greenbackpack=new Backpack("Green","large");
+
+// Open blue backpack
+bluebackpack.OpenBag();
+bluebackpack.Putin("Lunch");
+bluebackpack.Putin("Jacket");
+// Close blue backpack
+bluebackpack.CloseBag();
+bluebackpack.OpenBag();
+bluebackpack.Takeout("Jacket");
+bluebackpack.CloseBag();
+
+class Backpack{
+    public string Colour{get;set;}
+    public string Size{get;set;}
+    public List<string> items{get;set;}
+    public bool Open{get;set;}
+
+    public Backpack(string colour,string size){
+        this.Colour=colour;
+        this.Size=size;
+        this.items=new List<string>();
+        this.Open=false;
+    }
+
+// Function to open backpack
+    public void OpenBag(){
+        // Check if the backpack is closed or not  
+        if(Open == false ){    
+            Open=true;
+            Console.WriteLine(Colour+" backpack opened");
+
+        }
+        // If the backpack is already open
+        else{  
+            Console.WriteLine(Colour+" Backpack already opened");
+        }
+        
+    }
+    // Function to close bag
+    public void CloseBag(){
+        // Check if the backpack is closed or not
+        if(Open == true){
+            Open=false;
+            Console.WriteLine(Colour+" backpack closed");
+        }
+        // If the backpack is already open
+        else{   
+            Console.WriteLine(Colour+" Backpack Arleady closed");
+        }
+
+    }
+
+// Use a string to add items to backpack list
+    public void Putin(string usrinput){ 
+        // Check is bag is opened or not 
+        if(Open == true){   
+            items.Add(usrinput);
+            Console.WriteLine(usrinput+" Added in "+Colour+" Backpack");
+        }
+        // Display message if bag is closed
+        else{
+            Console.WriteLine("Backpack is closed please open it to add the item");
+        }
+    }
+
+// To check if the item is already in the list
+    int index=0;
+
+// Using a string to remove item from the backpack list
+    public void Takeout(string userinput){
+        if (Open == true){ 
+            // Loop through each item in the list
+            for(int i=0;i<items.Count;i++){
+                if (items[i] == userinput){
+                    // Remove the item if its in the list
+                    items.Remove(items[i]);  
+                    index=1;
+                    Console.WriteLine(userinput+" taken out");
+                }
+
+            }
+
+            if(index == 0){
+                Console.WriteLine(userinput+" not present in backpack");
+            }
+            // Set the index to 0 for nect iteration
+            index=0;
+        }
+        else{
+            Console.WriteLine("Backpack is closed please open it to remove item");
+        }
+    }
+}
---
+++ C#/DICE-ROLL-SIMULATOR-ASSIGNMENT/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int menuOption;
+
+            do
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Roll Dice Once");
+                Console.WriteLine("2. Roll Dice 5 Times");
+                Console.WriteLine("3. Roll Dice 'n' Times");
+                Console.WriteLine("4. Roll Dice until two 1's");
+                Console.WriteLine("5. Exit");
+
+                menuOption = int.Parse(Console.ReadLine());
+
+                switch (menuOption)
+                {
+                    case 1:
+                        int dice1 = rnd.Next(1, 7);
+                        int dice2 = rnd.Next(1, 7);
+                        int sum = dice1 + dice2;
+                        Console.WriteLine("You rolled a " + dice1 + " and a " + dice2 + " for a sum of " + sum);
+                        break;
+                    case 2:
+                        for (int i = 0; i < 5; i++)
+                        {
+                            int dice3 = rnd.Next(1, 7);
+                            int dice4 = rnd.Next(1, 7);
+                            int sum2 = dice3 + dice4;
+                            Console.WriteLine("You rolled a " + dice3 + " and a " + dice4 + " for a sum of " + sum2);
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the number of rolls:");
+                        int rolls = int.Parse(Console.ReadLine());
+                        for (int i = 0; i < rolls; i++)
+                        {
+                            int dice5 = rnd.Next(1, 7);
+                            int dice6 = rnd.Next(1, 7);
+                            int sum3 = dice5 + dice6;
+                            Console.WriteLine("You rolled a " + dice5 + " and a " + dice6 + " for a sum of " + sum3);
+                        }
+                        break;
+                    case 4:
+                        int dice7, dice8, rollsUntilDoubleOnes = 0;
+                        do
+                        {
+                            dice7 = rnd.Next(1, 7);
+                            dice8 = rnd.Next(1, 7);
+                            int sum4 = dice7 + dice8;
+                            Console.WriteLine("You rolled a " + dice7 + " and a " + dice8 + " for a sum of " + sum4);
+                            rollsUntilDoubleOnes++;
+                        } while (dice7 != 1 || dice8 != 1);
+                        Console.WriteLine("It took " + rollsUntilDoubleOnes + " rolls to roll two 1's");
+                        break;
+                    case 5:
+                        Console.WriteLine("Exiting program...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            } while (menuOption != 5);
+        }
+    }
+}
